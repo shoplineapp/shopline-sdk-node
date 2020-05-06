e814c32ae8d12509777b79fda622eeee4b6e507a
Eric Kwong <eric.kwong@shoplineapp.com>
2020-05-06 17:07:28 +0800

    feat(index): update export

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const DeveloperOAuth = require('./lib/models/DeveloperOAuth');
+const OpenApiClient = require('./lib/models/OpenApiClient');
 
 module.exports = {
   DeveloperOAuth,
+  OpenApiClient,
 }
