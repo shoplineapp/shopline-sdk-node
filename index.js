13de66d252c7225690cebba79a6a60a31b16939d
Terence Wu <terence@shoplineapp.com>
2022-03-01 10:04:34 +0800

    feat(AppBridge): implement app bridge

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const DeveloperOAuth = require('./lib/models/DeveloperOAuth');
+const AppBridge = require('./lib/models/AppBridge');
 const OpenApiClient = require('./lib/models/OpenApiClient');
 
 module.exports = {
   DeveloperOAuth,
   OpenApiClient,
+  AppBridge,
 }
