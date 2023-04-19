1498dee2a8f2d612172471ca60ceda56472e64d5
Jenny Chan <jenny.chan@shoplineapp.com>
2023-04-19 17:54:21 +0800

    chore: rename app-bridge to admin-embedded-app

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const DeveloperOAuth = require('./lib/models/DeveloperOAuth');
 const AppBridge = require('./lib/models/AppBridge');
+const AdminEmbeddedApp = require('./lib/models/AdminEmbeddedApp');
 const OpenApiClient = require('./lib/models/OpenApiClient');
 
 module.exports = {
   DeveloperOAuth,
   OpenApiClient,
+  AdminEmbeddedApp,
+  /** @deprecated */
   AppBridge,
 }
